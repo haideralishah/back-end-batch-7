4e1ae3988f872f31171d0c9d3a0f2a0ad9850704
haideralishah <headeralishah@gmail.com>
2021-12-01 10:58:07 +0500

    Mongoose added Mongoose connected

--- index.js
+++ index.js
@@ -110,32 +110,132 @@
 
 
 
+// import express from 'express';
+
+// const app = express();
+
+// app.use((req, res, next) => {
+//     // console.log(req.url);
+//     next();
+// });
+
+// app.get("/signup", (req, res, next) => {
+//     console.log('inside signup ');
+//     res.end('Signup run');
+// });
+
+// app.use((req, res) => {
+//     console.log('inside second middleware 1');
+//     res.end();
+// })
+
+// app.listen(7000, () => {
+//     console.log('=================== server started on 7000 ===================');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log(req.url);
-    next();
+mongoose.connect("DatabaseURL");
+mongoose.connection.once('open', () => {
+    console.log('=================== ISI Secrete Database Connected ===================');
+});
+mongoose.connection.on('error', () => {
+    console.log('=================== Black Vigo is outside your home ===================');
 });
 
-let users = [
-    {userName: 'haider', rollNumber:'2643'},
-    {userName: 'ali', rollNumber:'2644'},
-]
 
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post("/signup", (req, res, next) => {
-    console.log('inside signup ');
-    res.end('Signup run');
-});
 
 app.use((req, res) => {
-    console.log('inside second middleware 1');
+    console.log(req.body, '***************');
     res.end();
-})
-
-app.listen(3000, () => {
-    console.log('=================== server started on 3000 ===================');
 });
 
+app.listen('5000', () => {
+    console.log('=================== server started on 5000 ===================');
+});
